f77d4c6d826292a3ac411b35e1596929c0a3eb5f
zayanx7 <zayan1579@gmail.com>
2023-11-20 18:44:04 +0000

    Code

---
+++ app.py
@@ -0,0 +1,96 @@
+from flask import Flask, render_template, request
+import pandas as pd
+from sqlalchemy import create_engine
+import sqlite3
+import json
+
+app = Flask(__name__, static_url_path='/static')
+
+# Connect to SQLite database
+engine = create_engine('sqlite:///imdb.db')
+
+@app.route('/')
+def home():
+    # Connect to SQLite database
+    conn = sqlite3.connect('imdb.db')
+
+    # Query the data from the 'imdb_data' table
+    query = 'SELECT * FROM imdb_data'
+    movies_df = pd.read_sql_query(query, conn)
+
+    # Close the connection
+    conn.close()
+
+    # Convert DataFrame to a list of dictionaries for rendering in the template
+    movies = movies_df.to_dict(orient='records')
+
+    return render_template('index.html', movies=movies)
+
+@app.route('/movie/<series_title>')
+def movie_details(series_title):
+    # Connect to SQLite database
+    conn = sqlite3.connect('imdb.db')
+
+    # Query the data for the specific Series Title
+    query = f"SELECT * FROM imdb_data WHERE Series_Title = '{series_title}'"
+    movie_df = pd.read_sql_query(query, conn)
+
+    # Close the connection
+    conn.close()
+
+    # Convert DataFrame to a dictionary for rendering in the template
+    movie_data = movie_df.to_dict(orient='records')[0] if not movie_df.empty else None
+
+    return render_template('movie_details.html', movie_data=movie_data)
+
+@app.route('/search')
+def search():
+    search_query = request.args.get('search', '')
+    
+    # Connect to SQLite database
+    conn = sqlite3.connect('imdb.db')
+
+    # Query the data from the 'imdb_data' table based on the search query
+    query = f"SELECT * FROM imdb_data WHERE Series_Title LIKE '%{search_query}%'"
+    search_results_df = pd.read_sql_query(query, conn)
+
+    # Close the connection
+    conn.close()
+
+    # Convert DataFrame to a list of dictionaries for rendering in the template
+    search_results = search_results_df.to_dict(orient='records')
+
+    return render_template('search_results.html', search_results=search_results, search_query=search_query)
+
+@app.route('/dashboard')
+def dashboard():
+    # Connect to SQLite database
+    conn = sqlite3.connect('imdb.db')
+
+    # Query the data for IMDb Ratings Distribution
+    ratings_distribution_query = 'SELECT IMDB_Rating, COUNT(*) AS Count FROM imdb_data GROUP BY IMDB_Rating'
+    ratings_distribution_data = pd.read_sql_query(ratings_distribution_query, conn).to_dict(orient='records')
+    
+    
+
+    # Query the data for IMDb Ratings vs. Gross Revenue
+    ratings_vs_revenue_query = 'SELECT IMDB_Rating, Gross FROM imdb_data WHERE Gross IS NOT NULL'
+    ratings_vs_revenue_data = pd.read_sql_query(ratings_vs_revenue_query, conn).to_dict(orient='records')
+
+    # Query the data for Votes vs. Ratings
+    votes_vs_ratings_query = 'SELECT No_of_Votes, IMDB_Rating FROM imdb_data WHERE No_of_Votes IS NOT NULL'
+    votes_vs_ratings_data = pd.read_sql_query(votes_vs_ratings_query, conn).to_dict(orient='records')
+
+    # Close the connection
+    conn.close()
+
+    print('ratings_distribution_data:', ratings_distribution_data)
+
+
+    return render_template('dashboard.html',
+                           ratings_distribution_data=ratings_distribution_data,
+                           ratings_vs_revenue_data=ratings_vs_revenue_data,
+                           votes_vs_ratings_data=votes_vs_ratings_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
